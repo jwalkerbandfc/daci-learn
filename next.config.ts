a4eac18ba40b757c55621c4325f683ca40c51c9b
jwalkerbandfc <143511732+jwalkerbandfc@users.noreply.github.com>
2025-11-19 13:48:35 +0000

    Add redirect to Instructure Canvas course

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [{
+      source: '',
+      destination: 'https://canvas.instructure.com/courses/13420265',
+      permanent: true, // Use true for 308 (Permanent), false for 307 (Temporary)
+    },
+    ];
+  },
 };
 
 export default nextConfig;
