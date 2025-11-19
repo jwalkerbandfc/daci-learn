0f821e321a0b2a4e456762e652a7837f1a6ad05e
jwalkerbandfc <143511732+jwalkerbandfc@users.noreply.github.com>
2025-11-19 13:59:27 +0000

    Comment out Next.js link in footer
    Comment out the link to Next.js in the footer.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
         </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <!--<a
+        {/*<a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           target="_blank"
@@ -60,7 +60,7 @@ export default function Home() {
             height={16}
           />
           Go to nextjs.org →
-        </a>-->
+        </a>*/}
       </footer>
     </div>
   );
